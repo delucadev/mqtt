4ec77bd7cf858dd74fabe441f467164cadac40cb
manoel.vitor <manoelvitor@acad.ifma.edu.br>
2021-09-09 12:46:55 -0300

    input para receber o nome do cliente que será inscrito no topico especifico

--- subscribe.py
+++ subscribe.py
@@ -1,15 +1,18 @@
 import paho.mqtt.client as mqtt
 import time
 
+msg = ''
 def on_message(client, userdata, message):
-    print(f"Mensagem recebida: {message.payload.decode('utf-8')}")
-
+    print(f"Mensagem recebida: {message.payload.decode('utf-8')} ")
+    
+nome = input('nome do dispositivo: ')
 broker = 'mqtt.eclipseprojects.io'
-client = mqtt.Client('Dispositivo')
+client = mqtt.Client(nome)
 client.connect(broker)
 
 client.loop_start()
-client.subscribe("TEMPERATURA")
+client.subscribe("TemperaturaInterna")
 client.on_message = on_message
-time.sleep(60)
+
+time.sleep(120)
 client.loop_stop()
